2d2924bcc7a28838593dd6eb5236cf9b7f65d77a
Andreas Kleinbichler <andreas.kleinbichler@admiral.at>
2019-11-15 20:03:38 +0100

    Reorganize code structure.

---
+++ Training.HighViscosityFood.Tests/BugerTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using Moq;
+using FluentAssertions;
+using System;
+using Training.HighViscosityFood.Abstract;
+
+namespace Training.HighViscosityFood.Tests
+{
+    [TestFixture]
+    public class BugerTests
+    {
+        [Test]
+        public void ToBill_WithFoodProduct_BillListsCaloriesAndPriceFromFoodProduct()
+        {
+            int calories = 250;
+            int price = 12;
+            var orderTimeStamp = DateTime.Parse("11/13/2019 7:51:44 AM");
+            var expectedBill = new Bill()
+            {
+                Price = price,
+                Calories = calories,
+                OrderedAt = orderTimeStamp
+            };
+            var foodProductMock = new Mock<IFoodProduct>();
+            foodProductMock.Setup(p => p.GetCalories()).Returns(calories);
+            foodProductMock.Setup(p => p.GetPrice()).Returns(price);
+            var burger = new Burger(foodProductMock.Object, () => orderTimeStamp);
+
+            var bill = burger.ToBill();
+
+            bill.Should().BeEquivalentTo(expectedBill);
+        }
+    }
+}
